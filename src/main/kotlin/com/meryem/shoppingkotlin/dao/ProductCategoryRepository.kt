d05288b56baa67a4605eef32446e4ac6fe14fcb8
meryem.sekkat <meryem.sekkat@alithya.com>
2022-10-26 11:45:13 +0100

    fix: connecting data

--- src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductCategoryRepository.kt
+++ src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductCategoryRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.jpa.repository.JpaRepository
 import com.meryem.shoppingkotlin.entity.ProductCategory
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:3000")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 interface ProductCategoryRepository : JpaRepository<ProductCategory?, Long?>
--- src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductRepository.kt
+++ src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.data.jpa.repository.JpaRepository
 import com.meryem.shoppingkotlin.entity.Product
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:3000")
 interface ProductRepository : JpaRepository<Product?, Long?>
