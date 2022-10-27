cb6df4a8423fd00d5cd386175f01c34432cecf4f
meryem.sekkat <meryem.sekkat@alithya.com>
2022-10-27 12:29:15 +0100

    feat: add search

--- src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductRepository.kt
+++ src/main/kotlin/com/meryem/shoppingkotlin/dao/ProductRepository.kt
@@ -1,8 +1,20 @@
 package com.meryem.shoppingkotlin.dao
 
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.data.jpa.repository.JpaRepository
+
 import com.meryem.shoppingkotlin.entity.Product
+import org.springframework.data.domain.Page
+
+import org.springframework.data.domain.Pageable
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+import org.springframework.web.bind.annotation.CrossOrigin
+
+import org.springframework.web.bind.annotation.RequestParam
+
 
 @CrossOrigin("http://localhost:3000")
-interface ProductRepository : JpaRepository<Product?, Long?>
+interface ProductRepository : JpaRepository<Product?, Long?> {
+    fun findByCategoryId(@RequestParam("id") id: Long?, pageable: Pageable?): Page<Product?>?
+    fun findByNameContaining(@RequestParam("name") name: String?, pageable: Pageable?): Page<Product?>?
+}
