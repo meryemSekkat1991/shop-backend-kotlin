3fc1f4a437e416b3ee92c5cc26fa1af55a74bc14
meryem.sekkat <meryem.sekkat@alithya.com>
2022-11-02 20:01:08 +0100

    feat: show product

--- src/main/kotlin/com/meryem/shoppingkotlin/controller/ProductCategoryController.kt
+++ src/main/kotlin/com/meryem/shoppingkotlin/controller/ProductCategoryController.kt
@@ -2,6 +2,7 @@ package com.meryem.shoppingkotlin.controller
 
 import com.meryem.shoppingkotlin.entity.ProductCategory
 import com.meryem.shoppingkotlin.service.ProductCategoryService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:3000")
 class ProductCategoryController(private val ProductCategoryService: ProductCategoryService) {
     @GetMapping("/productCategories")
     fun getAll(): List<ProductCategory> = ProductCategoryService.getAll()
